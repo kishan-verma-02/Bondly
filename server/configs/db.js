69733bf7cb4d7395030c37006de04449e965c690
Kishan Verma <kishan7777verma@gmail.com>
2025-11-06 19:24:28 +0530

    this is my first social media app and first commit

---
+++ server/configs/db.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', ()=> console.log('Database connected'))
+        await mongoose.connect(`${process.env.MONGODB_URI}/bondly`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export default connectDB
